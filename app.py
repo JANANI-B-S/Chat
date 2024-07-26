baaa13bb11bca2732dd995dd0972e6fbc9b70545
JANANI-B-S <143819251+JANANI-B-S@users.noreply.github.com>
2024-07-26 23:19:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
+# Initialize ChatBot
 chatbot = ChatBot('ChatBot')
+
+# Train ChatBot with English language corpus
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-@app.route("/get")
+@app.route("/get", methods=['GET'])
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
-
+    user_text = request.args.get('msg')
+    if user_text:
+        response = chatbot.get_response(user_text)
+        return jsonify({"response": str(response)})
+    else:
+        return jsonify({"response": "No message provided"}), 400
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
